60c74a73c68456e3093345a99d30a676b0a40183
W8M2Hg9lLmWqXSGC <W8M2Hg9lLmWqXSGC@gmail.com>
2017-06-13 19:06:00 +1000

    Work in progress - * Mimic the original VB project. * Enumerate COM ports. * Simple HTML Help system. Should add Help icon / button. * Tidy Settings system. * Tabs used to seperate settings and additional utilities.

---
+++ Proxmark Tool/frmMain.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Proxmark_Tool
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tcMain = new System.Windows.Forms.TabControl();
+            this.tpMain = new System.Windows.Forms.TabPage();
+            this.tlpMain = new System.Windows.Forms.TableLayoutPanel();
+            this.tvMain = new System.Windows.Forms.TreeView();
+            this.tpSettings = new System.Windows.Forms.TabPage();
+            this.tvSettings = new DropDownTreeView.DropDownTreeView();
+            this.tcMain.SuspendLayout();
+            this.tpMain.SuspendLayout();
+            this.tlpMain.SuspendLayout();
+            this.tpSettings.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tcMain
+            // 
+            this.tcMain.Controls.Add(this.tpMain);
+            this.tcMain.Controls.Add(this.tpSettings);
+            this.tcMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tcMain.Location = new System.Drawing.Point(0, 0);
+            this.tcMain.Name = "tcMain";
+            this.tcMain.SelectedIndex = 0;
+            this.tcMain.Size = new System.Drawing.Size(457, 332);
+            this.tcMain.TabIndex = 1;
+            // 
+            // tpMain
+            // 
+            this.tpMain.Controls.Add(this.tlpMain);
+            this.tpMain.Location = new System.Drawing.Point(4, 22);
+            this.tpMain.Name = "tpMain";
+            this.tpMain.Padding = new System.Windows.Forms.Padding(3);
+            this.tpMain.Size = new System.Drawing.Size(449, 306);
+            this.tpMain.TabIndex = 0;
+            this.tpMain.Text = "Proxmark III";
+            this.tpMain.UseVisualStyleBackColor = true;
+            // 
+            // tlpMain
+            // 
+            this.tlpMain.ColumnCount = 2;
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpMain.Controls.Add(this.tvMain, 0, 0);
+            this.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpMain.Location = new System.Drawing.Point(3, 3);
+            this.tlpMain.Name = "tlpMain";
+            this.tlpMain.RowCount = 1;
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 300F));
+            this.tlpMain.Size = new System.Drawing.Size(443, 300);
+            this.tlpMain.TabIndex = 1;
+            // 
+            // tvMain
+            // 
+            this.tvMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvMain.Location = new System.Drawing.Point(3, 3);
+            this.tvMain.Name = "tvMain";
+            this.tvMain.Size = new System.Drawing.Size(215, 294);
+            this.tvMain.TabIndex = 0;
+            // 
+            // tpSettings
+            // 
+            this.tpSettings.Controls.Add(this.tvSettings);
+            this.tpSettings.Location = new System.Drawing.Point(4, 22);
+            this.tpSettings.Name = "tpSettings";
+            this.tpSettings.Padding = new System.Windows.Forms.Padding(3);
+            this.tpSettings.Size = new System.Drawing.Size(449, 306);
+            this.tpSettings.TabIndex = 1;
+            this.tpSettings.Text = "Settings";
+            this.tpSettings.UseVisualStyleBackColor = true;
+            // 
+            // tvSettings
+            // 
+            this.tvSettings.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvSettings.Location = new System.Drawing.Point(3, 3);
+            this.tvSettings.Name = "tvSettings";
+            this.tvSettings.Size = new System.Drawing.Size(443, 300);
+            this.tvSettings.TabIndex = 0;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 332);
+            this.Controls.Add(this.tcMain);
+            this.KeyPreview = true;
+            this.Name = "frmMain";
+            this.Text = "Proxmark Tool";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmMain_KeyDown);
+            this.tcMain.ResumeLayout(false);
+            this.tpMain.ResumeLayout(false);
+            this.tlpMain.ResumeLayout(false);
+            this.tpSettings.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tcMain;
+        private System.Windows.Forms.TabPage tpMain;
+        private System.Windows.Forms.TableLayoutPanel tlpMain;
+        private System.Windows.Forms.TreeView tvMain;
+        private System.Windows.Forms.TabPage tpSettings;
+        //private System.Windows.Forms.TreeView tvSettings;
+
+        private DropDownTreeView.DropDownTreeView tvSettings;
+    }
+}
+
---
+++ Proxmark Tool/frmMain.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Management;
+using System.Diagnostics;
+using DropDownTreeView;
+using System.IO;
+
+#region
+// Proxmark III Tool
+//
+// Original design stolen from Gaucho GUI
+//
+// Description: Provides a graphical user interface for the Proxmark III
+//
+// ============================================================================
+// This software is free software; you can modify and/or redistribute it, provided
+// that the authors are credited for the work.
+//
+// This library is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE.
+// ============================================================================
+#endregion
+
+namespace Proxmark_Tool
+{
+    public partial class frmMain : Form
+    {
+        public List<COMport> COMPorts = new List<COMport>();
+        public TreeNode tvSettingsNodes = new TreeNode("Settings");
+
+        private static Form Help = null;
+        private static WebBrowser browser = new WebBrowser();
+        public frmMain()
+        {
+            InitializeComponent();
+
+            COMPorts = getCOMports();
+            DropDownTreeNode ddtnCOMport = new DropDownTreeNode("COM1");
+            ddtnCOMport.ComboBox.Width = 200;
+            ddtnCOMport.ComboBox.MinimumSize = new Size(200,12);
+            TreeNode tnCOMport = new TreeNode("COM Port");
+            tnCOMport.Nodes.Add(ddtnCOMport);
+            tvSettingsNodes.Nodes.Add(tnCOMport);
+            //tvSettingsNodes.Nodes.Add("Some other setting.");
+            tvSettings.Nodes.Add(tvSettingsNodes);
+
+            tvSettings.ExpandAll();
+
+            if (COMPorts.Count > 0)
+            {
+
+                foreach (COMport port in COMPorts)
+                {
+                    ddtnCOMport.ComboBox.Items.Add("(" + port.DeviceID + ") " +
+                        port.Description);
+
+                }
+                if (COMPorts.Exists(x => x.Description.Contains("Proxmark3")))
+                    Trace.WriteLine("Description Match");
+                if (COMPorts.Exists(x => x.PNPDeviceID.Contains("PROXMARK.ORG")))
+                    Trace.WriteLine("PNPDeviceID Match");
+                //COMPorts.Contains(ConfigurationManager.AppSettings["SerialPort"])
+
+            }
+            else
+            {
+                Trace.WriteLine("No available ports");
+                Application.Exit();
+            }
+        }
+
+        public List<COMport> getCOMports()
+        {
+            List<COMport> _result = new List<COMport>();
+            try
+            {
+                using (var query = new ManagementObjectSearcher(@"root\CIMV2", "SELECT * FROM Win32_SerialPort"))
+                    foreach (ManagementObject queryItem in query.Get())
+                        _result.Add(new COMport() {
+                            DeviceID    = queryItem.GetPropertyValue("DeviceID").ToString(),
+                            Description = queryItem.GetPropertyValue("Description").ToString(),
+                            PNPDeviceID = queryItem.GetPropertyValue("PNPDeviceID").ToString() });
+            }
+            catch (Exception _ex)
+            {
+                Trace.WriteLine(_ex.Message);
+                Application.Exit();
+            }
+
+            return _result;
+        }
+
+        private void frmMain_KeyDown(object sender, KeyEventArgs e)
+        {
+            Trace.WriteLine(e.KeyCode.ToString());
+            if (e.KeyCode == Keys.F1)
+                ShowHelp();
+        }
+
+        private void ShowHelp()
+        {
+            if (Help != null)
+            {
+                Help.BringToFront();
+            }
+            else
+            {
+                Help = new Form();
+                Help.Show();
+            }
+
+            string curDir = Directory.GetCurrentDirectory();
+
+            browser.Dock                           = DockStyle.Fill;
+            browser.AllowNavigation                = false;
+            browser.AllowWebBrowserDrop            = false;
+            browser.IsWebBrowserContextMenuEnabled = false;
+            browser.Url                            = new Uri(String.Format("file:///{0}/Documentation/index.html", curDir));
+
+            Help.Controls.Add(browser);
+        }
+
+    }
+
+
+
+    public class COMport
+    {
+        public string Description; //Proxmark3
+        public string DeviceID;
+        public string PNPDeviceID; //PROXMARK.ORG
+    }
+
+}
